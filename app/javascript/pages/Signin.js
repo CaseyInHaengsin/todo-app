e612906bb23a0d5aef7a9a913724dad3abf7a172
CaseyInHaengsin <9088410+CaseyInHaengsin@users.noreply.github.com>
2022-02-14 07:58:29 -0700

    adjusted routes a bit.

--- app/javascript/pages/Signin.js
+++ app/javascript/pages/Signin.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 function Signin () {
-  return <div>Signin</div>
+  return (
+    <div className='flex justify-center'>
+      <h1>Signin</h1>
+    </div>
+  )
 }
 
 export default Signin
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root 'home#index'
   namespace :api, defaults: { format: 'json' } do
     resources :tasks
   end
+  root 'home#index'
+  get '/*path' => 'home#index'
 end
